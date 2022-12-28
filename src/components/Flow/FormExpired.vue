fd2c52ffee06b6f0a793dd355f9f431aa473186c
Ferdinand Thiessen <rpm@fthiessen.de>
2022-12-28 01:40:02 +0100

    Added Workflow UI for the forms check
    Signed-off-by: Ferdinand Thiessen <rpm@fthiessen.de>

---
+++ src/components/Flow/FormExpired.vue
@@ -0,0 +1,21 @@
+<template>
+	<div />
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'FormExpired',
+    props: {
+		value: {
+			type: String,
+			default: '',
+		},
+		check: {
+			type: Object,
+			default: () => { return {} },
+		},
+	},
+	setup() {},
+})
+</script>
--- webpack.js
+++ webpack.js
@@ -4,5 +4,6 @@ const webpackConfig = require('@nextcloud/webpack-vue-config')
 webpackConfig.entry.emptyContent = path.resolve(path.join('src', 'emptyContent.js'))
 webpackConfig.entry.submit = path.resolve(path.join('src', 'submit.js'))
 webpackConfig.entry.settings = path.resolve(path.join('src', 'settings.js'))
+webpackConfig.entry.workflow = path.resolve(path.join('src', 'workflow.js'))
 
 module.exports = webpackConfig
